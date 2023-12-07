ee2bc44829422275b6a635228598bc13a5336127
AshutoshDas <ashutosh.rkl.2001@gmail.com>
2023-12-07 10:38:43 +0530

    updated

--- src/com/BeanLifeCycle/Test.java
+++ src/com/BeanLifeCycle/Test.java
@@ -1,12 +1,17 @@
 package com.BeanLifeCycle;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import java.sql.SQLException;
 
 public class Test
 {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        StudentDAO dao = new StudentDAO();
-        dao.selectAllRows();
-        dao.deleteStudentRecord("B@gmail.com");
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/BeanLifeCycle/beans.xml");
+        StudentDAO s = context.getBean("dao", StudentDAO.class);
+        s.selectAllRows();
+
     }
 }
